e0350404acfda6eb0fc7c279522e084cabde538d
kulin1987 <kulin1987@gmail.com>
2014-06-22 18:04:09 +0300

    Formatted class scoreboard
    Formatted class scoreboard with StyleCop

--- Scoreboard.cs
+++ Scoreboard.cs
@@ -1,41 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿//-----------------------------------------------------------------------
+// <copyright file="Scoreboard.cs" company="TeamSupreme">
+//     Copyright TeamSupreme. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace GameFifteenProject
 {
-    static class Scoreboard
-    {
-        private static List<Player> players = new List<Player>();
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
+    /// <summary>
+    /// Display the scoreboard
+    /// </summary>
+    public static class Scoreboard
+    {
+        /// <summary>
+        /// List with all players
+        /// </summary>
+        private static readonly List<Player> Players = new List<Player>();
+
+        /// <summary>
+        /// Method to add players
+        /// </summary>
+        /// <param name="player">Current player which we want to add to the scoreboard</param>
         public static void AddPlayer(Player player)
         {
-            players.Add(player);
-            players.Sort();
+            Players.Add(player);
+            Players.Sort();
             DeleteAllExceptTopPlayers();
         }
         
+        /// <summary>
+        /// Method to display all players in the scoreboard
+        /// </summary>
         public static void PrintScoreboard()
         {
             Console.WriteLine("Scoreboard:");
-            foreach (Player player in players)
+            foreach (Player player in Players)
             {
-                string scoreboardLine = (players.IndexOf(player) + 1).ToString() + ". " + player.Name + " --> " + player.Moves.ToString() + " moves";
+                string scoreboardLine = (Players.IndexOf(player) + 1).ToString() + ". " + player.Name + " --> " + player.Moves.ToString() + " moves";
                 Console.WriteLine(scoreboardLine);
             }
         }
 
+        /// <summary>
+        /// Method to delete players
+        /// </summary>
         private static void DeleteAllExceptTopPlayers()
         {
-            for (int index = 0; index < players.Count(); index++)
+            for (int index = 0; index < Players.Count(); index++)
             {
                 if (index > 4)
                 {
-                    players.Remove(players[index]);
+                    Players.Remove(Players[index]);
                 }
             }
         }
-
     }
 }
