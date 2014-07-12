2db1d94fd648c15c54212505daf69db9fe75068a
Imraotnh <nkar@mail.bg>
2014-07-12 08:41:10 +0300

    Refactored Tile class

--- Tile.cs
+++ Tile.cs
@@ -1,36 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿//-----------------------------------------------------------------------
+// <copyright file="Tile.cs" company="TeamSupreme">
+//     Copyright TeamSupreme. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace GameFifteenProject
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// Class used to manage tiles
+    /// </summary>
     public class Tile : IComparable
     {
+        /// <summary>
+        /// String showing the label of the tile
+        /// </summary>
         private string label;
+
+        /// <summary>
+        /// Integer showing the position of the tile
+        /// </summary>
         private int position;
 
-        public string Label
+        /// <summary>
+        /// Initializes a new instance of the Tile class.
+        /// </summary>
+        public Tile()
         {
-            get { return label; }
         }
 
-        public int Position
-        {
-            get { return position; }
-            set { position = value; }
-        }
-
+        /// <summary>
+        /// Initializes a new instance of the Tile class.
+        /// </summary>
+        /// <param name="label">Showing the label of the tile</param>
+        /// <param name="position">Showing the position of the tile</param>
         public Tile(string label, int position)
         {
             this.label = label;
             this.position = position;
         }
 
-        public Tile()
+        /// <summary>
+        /// Gets label of the tile.
+        /// </summary>
+        public string Label
         {
+            get { return this.label; }
         }
 
+        /// <summary>
+        /// Gets or sets the position of the tile.
+        /// </summary>
+        public int Position
+        {
+            get { return this.position; }
+            set { this.position = value; }
+        }
+
+        /// <summary>
+        /// Compares the positions of two tiles
+        /// </summary>
+        /// <param name="tile">Second tile</param>
+        /// <returns>The tile with bigger position</returns>
         public int CompareTo(object tile)
         {
             Tile currentTile = (Tile)tile;
