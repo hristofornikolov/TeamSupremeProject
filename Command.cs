921476e904b6e0d3ce44ccd8d28ae3fb995c2c32
Imraotnh <nkar@mail.bg>
2014-07-12 15:37:04 +0300

    Refactored Command class

--- Command.cs
+++ Command.cs
@@ -1,14 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿//-----------------------------------------------------------------------
+// <copyright file="Command.cs" company="TeamSupreme">
+//     Copyright TeamSupreme. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
 
 namespace GameFifteenProject
 {
-    static class Command
-    {
-        private enum Commands { restart, top, exit };
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
 
+    /// <summary>
+    /// Class that manages the commands
+    /// </summary>
+    public static class Command
+    {
+        /// <summary>
+        /// All possibilities for the commands
+        /// </summary>
+        private enum Commands 
+        { 
+            /// <summary>
+            /// Command for restarting the game
+            /// </summary>
+            restart,
+
+            /// <summary>
+            /// Command for showing the top players
+            /// </summary>
+            top,
+
+            /// <summary>
+            /// Command for exiting the game
+            /// </summary>
+            exit 
+        }
+
+        /// <summary>
+        /// Method that takes the command from the user
+        /// </summary>
+        /// <param name="input">Command written by the user</param>
+        /// <returns>The command if it is valid, 
+        /// exception elsewhere</returns>
         public static string CommandType(string input)
         {
             string inputToLower = input.ToLower();
@@ -25,6 +59,5 @@ namespace GameFifteenProject
 
             return output;
         }
-
     }
 }
