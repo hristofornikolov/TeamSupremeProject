ac0813c477516ff4c7638ef816ee04a6fe8fd33c
hristofornikolov <hristofor.nikolov@yahoo.co.uk>
2014-07-21 16:45:30 +0300

    Unit test addition to the FieldMatrix class

--- GameFifteen.Test/UnitTestFieldMatrix.cs
+++ GameFifteen.Test/UnitTestFieldMatrix.cs
@@ -2,10 +2,22 @@
 {
     using System;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using GameFifteenProject.GameObjects;
 
     [TestClass]
     public class UnitTestFieldMatrix
     {
-        
+        [TestMethod]
+        public void TestNewFieldMatrixConstructorCorrectSize()
+        {
+            FieldMatrix fieldMatrix = new FieldMatrix();
+            Assert.AreEqual(fieldMatrix.Rows, 4);
+            Assert.AreEqual(fieldMatrix.Columns, 4);
+
+        }
+
+
+
+
     }
 }
