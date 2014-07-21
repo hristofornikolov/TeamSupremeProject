f8c7f8215c2e8480eb334d7170f2332b906b3562
hristofornikolov <hristofor.nikolov@yahoo.co.uk>
2014-07-21 16:58:48 +0300

    Another unit test addition to the FieldMatrix class

--- GameFifteen.Test/UnitTestFieldMatrix.cs
+++ GameFifteen.Test/UnitTestFieldMatrix.cs
@@ -13,10 +13,24 @@
             FieldMatrix fieldMatrix = new FieldMatrix();
             Assert.AreEqual(fieldMatrix.Rows, 4);
             Assert.AreEqual(fieldMatrix.Columns, 4);
-
         }
 
+        [TestMethod]
+        public void TestNewFieldMatrixConstructorCorrectInitialization()
+        {
+            FieldMatrix fieldMatrix = new FieldMatrix();
+            for (int i = 0; i < fieldMatrix.Rows; i++)
+            {
+                for (int j = 0; j < fieldMatrix.Columns; j++)
+                {
+                    if (fieldMatrix[i, j] != 0)
+                    {
+                        throw new ArgumentException("When initialized, a cell value must be equal to zero!");
+                    }
+                }
+            }
 
+        }
 
 
     }
