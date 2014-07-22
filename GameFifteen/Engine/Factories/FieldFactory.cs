00ec694ef48f836928c779a3b2ab7a2a5678a0a8
kulin1987 <kulin1987@gmail.com>
2014-07-22 17:36:14 +0300

    Options for change matrix size
    Add more cases in FieldFactory.cs for changing the size of the field
    marix

--- GameFifteen/Engine/Factories/FieldFactory.cs
+++ GameFifteen/Engine/Factories/FieldFactory.cs
@@ -32,6 +32,10 @@
             {
                 case 4:
                     return new FieldMatrix(4, 4);
+                case 5:
+                    return new FieldMatrix(5, 5);
+                case 6:
+                    return new FieldMatrix(6, 6);
                 default:
                     return new FieldMatrix(4, 4);
             }
