1ba26eeacb2ea99fa04d408c09dbb2240d493087
kulin1987 <kulin1987@gmail.com>
2014-07-23 00:56:06 +0300

    Unit test for class Command.cs and RestartCommand.cs
    Adding unit test methods for classes Command.cs and RestartCommand.cs

---
+++ GameFifteen.Tests/UnitTestRestartCommands.cs
@@ -0,0 +1,48 @@
+﻿namespace GameFifteen.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using GameFifteen.Contracts;
+    using GameFifteen.Contracts.Engine;
+    using GameFifteen.Engine.Factories;
+    using GameFifteen.Engine;
+    using GameFifteen.GameObjects;
+    using GameFifteen.Extensions;
+
+    /// <summary>
+    /// Summary description for UnitTestCommands
+    /// </summary>
+    [TestClass]
+    public class UnitTestRestartCommands
+    {
+        [TestMethod]
+        public void CheckIfCommandConstructorWorksProperly()
+        {
+            IFieldMatrix field = FieldFactory.Instance.GetField(5);
+            IRenderer renderer = new ConsoleRenderer();
+            IScoreboard scoreboard = new ScoreboardProxy();
+            IRandomNumberGenerator random = new RandomNumberGenerator();
+            IGameEngine gameEngine = new GameFifteenEngine(field, renderer, scoreboard, random);
+
+            RestartCommand command = new RestartCommand(gameEngine);
+
+            Assert.IsInstanceOfType(command, typeof(Command));
+        }
+
+        [TestMethod]
+        public void CheckIfMethodExecuteReturnsProperString()
+        {
+            IFieldMatrix field = FieldFactory.Instance.GetField(5);
+            IRenderer renderer = new ConsoleRenderer();
+            IScoreboard scoreboard = new ScoreboardProxy();
+            IRandomNumberGenerator random = new RandomNumberGenerator();
+            IGameEngine gameEngine = new GameFifteenEngine(field, renderer, scoreboard, random);
+
+            RestartCommand command = new RestartCommand(gameEngine);
+
+            string result = command.Execute();
+
+            Assert.IsInstanceOfType(result, typeof(String));
+        }
+    }
+}
