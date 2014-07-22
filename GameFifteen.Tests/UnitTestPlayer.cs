28df6e422b6a47e8c0aa138095eed3f8c22fba90
kulin1987 <kulin1987@gmail.com>
2014-07-22 16:37:27 +0300

    Unit test for class Player
    Add more Unit tests for checking Players moves and name.

--- GameFifteen.Tests/UnitTestPlayer.cs
+++ GameFifteen.Tests/UnitTestPlayer.cs
@@ -3,6 +3,7 @@
     using System;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using GameFifteen.GameObjects;
+    using GameFifteen.Engine;
 
     /// <summary>
     /// Summary description for UnitTestPlayer
@@ -11,11 +12,38 @@
     public class UnitTestPlayer
     {
         [TestMethod]
-        public void CheckIfPlayerNameIsValid()
+        public void CheckIfPlayerNameIsValidString()
         {
            Player player = new Player("Pesho", 5);
 
            Assert.AreSame("Pesho", player.Name);
+        }
+
+        [TestMethod]
+        public void CheckIfPlayerNameIsNull()
+        {
+            string playerName = null;
+
+            Player player = new Player(playerName, 5);
+
+            Assert.AreSame("Unknown", player.Name);
+        }
+
+        [TestMethod]
+        public void CheckIfPlayerNameIsEmpty()
+        {
+            string playerName = "";
+
+            Player player = new Player(playerName, 5);
+
+            Assert.AreSame("Unknown", player.Name);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CheckIfPlayerMovesAreValid()
+        {
+            Player player = new Player("Pesho", -5);
         }
     }
 }
